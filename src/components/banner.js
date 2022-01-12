ad08c85dd38db423dd571673df58e9c86323a432
nhatnvph15151 <nhatnvph15151@fpt.edu.vn>
2022-01-12 12:39:52 +0700

    first commit

---
+++ src/components/banner.js
@@ -0,0 +1,7 @@
+const Banner = {
+    render (){
+        return /* html */`
+        <img class="w-auto h-auto object-fill" src="https://fptnamdinh.vn/wp-content/uploads/2020/12/banner.jpg" alt="">`;
+    },
+};
+export default Banner;
